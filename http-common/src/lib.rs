78f7e1d83bacb07afbcad121967eab1d004dc0ed
Daniel Prilik <daprilik@microsoft.com>
2020-10-19 19:09:18 -0400

    sort those clippy lints

--- http-common/src/lib.rs
+++ http-common/src/lib.rs
@@ -9,8 +9,8 @@
 	clippy::module_name_repetitions,
 	clippy::must_use_candidate,
 	clippy::similar_names,
-	clippy::type_complexity,
 	clippy::too_many_lines,
+	clippy::type_complexity,
 )]
 
 mod dynrange;
