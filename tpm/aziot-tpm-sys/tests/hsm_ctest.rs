360684bd5da9d2309406091ac590860dff31a7a0
Daniel Prilik <daprilik@microsoft.com>
2020-11-10 15:55:12 -0500

    fix tests

--- tpm/aziot-tpm-sys/tests/hsm_ctest.rs
+++ tpm/aziot-tpm-sys/tests/hsm_ctest.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-#![deny(rust_2018_idioms, warnings)]
-#![deny(clippy::all, clippy::pedantic)]
+#![deny(rust_2018_idioms)]
+#![warn(clippy::all, clippy::pedantic)]
 //Skip ARM(cross-compile) until I figure out how to run ctest on this.
 #![cfg(not(any(target_arch = "arm", target_arch = "aarch64")))]
 
