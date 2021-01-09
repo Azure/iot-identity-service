1373b3a19121c0a9c6f8438c0dc8cc1db975df0b
Rajeev Massand <massand@gmail.com>
2021-01-09 06:35:56 +0000

    fixes

--- identity/aziot-identityd/src/lib.rs
+++ identity/aziot-identityd/src/lib.rs
@@ -85,47 +85,40 @@ pub async fn main(
     });
 
     let api_watcher = api.clone();
-
     let (file_changed_tx, mut file_changed_rx) = tokio::sync::mpsc::channel(10);
-
     let config_directory_path_copy = config_directory_path.clone();
     
-    // Start file watcher
+    // Start file watcher using blocking channel
     std::thread::spawn(move || {
         
-        // Create a channel to receive the events.
         let (file_watcher_tx, file_watcher_rx) = std::sync::mpsc::channel();
 
-        // Create a watcher object, delivering debounced events.
-        // The notification back-end is selected based on the platform.
+        // Create a watcher object, delivering debounced events
         let mut file_watcher = notify::watcher(file_watcher_tx, std::time::Duration::from_secs(10)).unwrap();
 
-        // Add a path to be watched. All files and directories at that path and
-        // below will be monitored for changes.
+        // Add configuration path to be watched
         file_watcher.watch(config_directory_path_copy, notify::RecursiveMode::Recursive).unwrap();
 
         loop {
             let _ = file_watcher_rx.recv();
-            
             let _ = file_changed_tx.blocking_send(());
         }
     });
 
-    // Start file watcher
+    // Start file change listener that asynchronously updates Identity Service
     tokio::task::spawn(async move {
         loop {
             let event = file_changed_rx.recv().await;
             
             match event {
                 Some(_) => {
-                    config_common::read_config(config_path.clone(), config_directory_path.clone())
-                    .map(|new_settings: config::Settings| async {
-                        let mut api_ = api_watcher.lock().await;
-                        let _ = api_
-                            .update_settings(new_settings, ReprovisionTrigger::ConfigurationFileUpdate)
-                            .await;
-                        drop(api_);
-                    });
+                    let new_settings = config_common::read_config(config_path.clone(), config_directory_path.clone()).unwrap();
+                    
+                    let mut api_ = api_watcher.lock().await;
+                    let _ = api_
+                        .update_settings(new_settings, ReprovisionTrigger::ConfigurationFileUpdate)
+                        .await;
+                    drop(api_);
                 },
                 None => {},
             }
