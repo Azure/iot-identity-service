b9ff9f81d134b6f298911c7f3c2034bb7174d9bc
Daniel Prilik <daprilik@microsoft.com>
2021-01-25 15:52:38 -0500

    a few misc. fixes

--- aziot/src/internal/check/checks/hostname.rs
+++ aziot/src/internal/check/checks/hostname.rs
@@ -61,8 +61,8 @@ impl Hostname {
             && !config_hostname.starts_with(&format!("{}.", machine_hostname))
         {
             return Err(anyhow!(
-                "config.yaml has hostname {} but device reports hostname {}.\n\
-                 Hostname in config.yaml must either be identical to the device hostname \
+                "identityd.toml has hostname {} but device reports hostname {}.\n\
+                 Hostname in identityd.toml must either be identical to the device hostname \
                  or be a fully-qualified domain name that has the device hostname as the first component.",
                 config_hostname, machine_hostname,
             ));
@@ -72,7 +72,7 @@ impl Hostname {
         // For example, the IoT Hub C# SDK cannot connect to a hostname that contains an `_`.
         if !is_rfc_1035_valid(config_hostname) {
             return Ok(CheckResult::Warning(anyhow!(
-                "config.yaml has hostname {} which does not comply with RFC 1035.\n\
+                "identityd.toml has hostname {} which does not comply with RFC 1035.\n\
                  \n\
                  - Hostname must be between 1 and 255 octets inclusive.\n\
                  - Each label in the hostname (component separated by \".\") must be between 1 and 63 octets inclusive.\n\
@@ -86,7 +86,7 @@ impl Hostname {
 
         if !check_length_for_local_issuer(config_hostname) {
             return Ok(CheckResult::Warning(anyhow!(
-                "config.yaml hostname {} is too long to be used as a certificate issuer",
+                "identityd.toml hostname {} is too long to be used as a certificate issuer",
                 config_hostname,
             )));
         }
