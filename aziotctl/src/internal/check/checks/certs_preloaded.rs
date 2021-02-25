62974c8c030f863eb993e4b21e8930dbd5648eb7
Arnav Singh <arsing@microsoft.com>
2021-02-24 22:20:14 -0800

    Fix preloaded-certs-validity check to correctly handle lists where not every element is another preloaded cert.

--- aziotctl/src/internal/check/checks/certs_preloaded.rs
+++ aziotctl/src/internal/check/checks/certs_preloaded.rs
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+use std::collections::BTreeMap;
+
 use anyhow::{anyhow, Result};
 use serde::Serialize;
+use url::Url;
 
 use crate::internal::check::util::CertificateValidityExt;
 use crate::internal::check::{CheckResult, Checker, CheckerCache, CheckerMeta, CheckerShared};
@@ -36,41 +39,65 @@ impl CertsPreloaded {
     ) -> Result<CheckResult> {
         let preloaded_certs = &unwrap_or_skip!(&cache.cfg.certd).preloaded_certs;
 
-        // TODO?: support returning multiple check results from a single check
-        // this will require some non-trivial changes to the checker framework, as currently
-        // there isn't any way to return a _dynamic_ number of results from a single check.
-        for (id, cert) in preloaded_certs {
-            match cert {
-                PreloadedCert::Ids(ids) => {
-                    // validate that the ids correspond to other preloaded certs
-                    for inner_id in ids {
-                        if preloaded_certs.get(inner_id).is_none() {
-                            return Err(anyhow!(
-                                "id '{}' in '{}' does not point to a valid cert",
-                                inner_id,
-                                id
-                            ));
-                        };
-                    }
-                }
-                PreloadedCert::Uri(uri) => {
-                    if uri.scheme() != "file" {
-                        return Err(anyhow!(
-                            "only file:// schemes are supported for preloaded certs."
-                        ));
-                    }
+        let mut visited: BTreeMap<_, _> = Default::default();
 
-                    match CertificateValidity::new(uri.path(), "", &id)
-                        .await?
-                        .to_check_result()?
-                    {
-                        CheckResult::Ok => {}
-                        res => return Ok(res),
-                    }
-                }
-            };
+        for id in preloaded_certs.keys() {
+            match walk_preloaded_certs(id, preloaded_certs, &mut visited).await? {
+                CheckResult::Ok => {}
+                res => return Ok(res),
+            }
         }
 
         Ok(CheckResult::Ok)
     }
+}
+
+#[async_recursion::async_recursion]
+async fn walk_preloaded_certs<'a>(
+    id: &'a str,
+    preloaded_certs: &'a BTreeMap<String, PreloadedCert>,
+    visited: &mut BTreeMap<&'a str, &'a Url>,
+) -> Result<CheckResult> {
+    match preloaded_certs.get(id) {
+        Some(PreloadedCert::Uri(uri)) => match visited.insert(id, uri) {
+            Some(previous_uri) if previous_uri != uri => {
+                return Err(anyhow!(
+                    "preloaded cert {:?} has been defined more than once",
+                    id,
+                ))
+            }
+
+            Some(_) => (),
+
+            None => {
+                if uri.scheme() != "file" {
+                    return Err(anyhow!(
+                        "preloaded cert {:?} has a scheme other than `file://`",
+                        id,
+                    ));
+                }
+
+                match CertificateValidity::new(uri.path(), "", &id)
+                    .await?
+                    .to_check_result()?
+                {
+                    CheckResult::Ok => {}
+                    res => return Ok(res),
+                }
+            }
+        },
+
+        Some(PreloadedCert::Ids(ids)) => {
+            for id in ids {
+                match walk_preloaded_certs(id, preloaded_certs, visited).await? {
+                    CheckResult::Ok => {}
+                    res => return Ok(res),
+                }
+            }
+        }
+
+        None => (),
+    }
+
+    Ok(CheckResult::Ok)
 }
