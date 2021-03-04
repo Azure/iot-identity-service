0eaf0b80775758ef8ead4e38864f13de43d86a21
Arnav Singh <arsing@microsoft.com>
2021-03-03 21:38:03 -0800

    Remove `aziotctl config wizard`
    We determined such a prompt-based wizard is too hard to use. For now,
    we're shipping the super-config approach used with `aziotctl config apply`,
    and are looking to add more focused wizards like `aziotctl config mp` and
    `aziotctl config dps` for manual and DPS provisioning respectively
    in the future.
    
    As part of removing the mentions of `aziotctl config wizard`
    from `packaging.md`, this commit also removes the IoT Edge-specific text from
    that document. This text will be moved to a matching document in
    the iotedge repo and be maintained there.
    
    Co-authored-by: Micah Lewis <1009123+micahl@users.noreply.github.com>

--- aziotctl/src/config/mod.rs
+++ aziotctl/src/config/mod.rs
@@ -1,20 +1,15 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 mod apply;
-mod wizard;
 
 #[derive(structopt::StructOpt)]
 pub(crate) enum Options {
     /// Apply the configuration to the Azure IoT Identity Service and related services.
     Apply(apply::Options),
-
-    /// Interactive wizard to get the Azure IoT Identity Service and related services up and running.
-    Wizard,
 }
 
 pub(crate) fn run(options: Options) -> anyhow::Result<()> {
     match options {
         Options::Apply(options) => apply::run(options),
-        Options::Wizard => wizard::run(),
     }
 }
