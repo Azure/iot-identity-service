2d7ccb93d03fb1222c8bdb8b03486d90d18e27d0
Rajeev Massand <massand@gmail.com>
2021-03-24 23:10:01 -0700

    Add check for preventing DPS provisioning in nested mode (#180)
    Nested Edge deployments only support IoT Hub provisioning for release/1.2. This PR introduces informative errors on invoking `aziotctl config apply` and daemon logs, if Identity Service is configured with `parent_hostname` AND DPS provisioning parameters.

--- identity/aziot-identityd/src/error.rs
+++ identity/aziot-identityd/src/error.rs
@@ -6,6 +6,7 @@ pub enum Error {
     Authorization,
     DeviceNotFound,
     DPSClient(std::io::Error),
+    DPSNotSupportedInNestedMode,
     HubClient(std::io::Error),
     KeyClient(std::io::Error),
     ModuleNotFound,
@@ -29,6 +30,9 @@ impl std::fmt::Display for Error {
             Error::Authorization => f.write_str("authorization error"),
             Error::DeviceNotFound => f.write_str("device identity not found"),
             Error::DPSClient(_) => f.write_str("DPS client error"),
+            Error::DPSNotSupportedInNestedMode => {
+                f.write_str("DPS provisioning is not supported in nested mode")
+            }
             Error::HubClient(_) => f.write_str("Hub client error"),
             Error::KeyClient(_) => f.write_str("Key client error"),
             Error::ModuleNotFound => f.write_str("module identity not found"),
@@ -46,6 +50,7 @@ impl std::error::Error for Error {
             Error::Authentication
             | Error::Authorization
             | Error::DeviceNotFound
+            | Error::DPSNotSupportedInNestedMode
             | Error::ModuleNotFound => None,
             Error::DPSClient(err) | Error::HubClient(err) | Error::KeyClient(err) => Some(err),
             Error::Internal(err) => Some(err),
