fd694bbc621db5981d7c096941ed4a54fca4c718
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-06-17 10:20:48 -0700

    clippy unusual_byte_grouping to inconsistent_digit_grouping

--- openssl-build/src/lib.rs
+++ openssl-build/src/lib.rs
@@ -10,7 +10,7 @@ pub fn define_version_number_cfg() {
         .expect("DEP_OPENSSL_VERSION_NUMBER must have been set by openssl-sys");
     let openssl_version = u64::from_str_radix(&openssl_version, 16)
         .expect("DEP_OPENSSL_VERSION_NUMBER must have been set to a valid integer");
-    #[allow(clippy::unusual_byte_groupings)]
+    #[allow(clippy::inconsistent_digit_grouping)]
     {
         if openssl_version >= 0x01_01_00_00_0 {
             println!("cargo:rustc-cfg=ossl110");
