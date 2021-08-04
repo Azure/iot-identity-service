7247e00502b08179a12babde1888f7eff93583b4
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-08-04 10:43:41 -0700

    Add chunked server response (#254)
    Adds support for chunked server responses. This is used by edged's module logs API.

--- http-common/src/server.rs
+++ http-common/src/server.rs
@@ -342,6 +342,27 @@ pub mod response {
         res
     }
 
+    pub fn chunked<S, O, E>(
+        status_code: hyper::StatusCode,
+        body: S,
+        content_type: &'static str,
+    ) -> hyper::Response<hyper::Body>
+    where
+        S: futures_util::stream::Stream<Item = Result<O, E>> + Send + 'static,
+        O: Into<hyper::body::Bytes> + 'static,
+        E: Into<Box<dyn std::error::Error + Send + Sync>> + 'static,
+    {
+        let body = hyper::Body::wrap_stream(body);
+
+        let res = hyper::Response::builder()
+            .status(status_code)
+            .header(hyper::header::CONTENT_TYPE, content_type)
+            .body(body);
+
+        let res = res.expect("cannot fail to build hyper response");
+        res
+    }
+
     pub fn json(
         status_code: hyper::StatusCode,
         body: &impl serde::Serialize,
