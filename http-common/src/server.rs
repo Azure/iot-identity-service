1e8e1be6b0b19375e269467b0fd5730e72c5a353
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-09-20 17:17:44 -0700

    http server: don't try and parse empty bodies

--- http-common/src/server.rs
+++ http-common/src/server.rs
@@ -96,24 +96,27 @@ macro_rules! make_service {
                                 return Box::pin(async move {
                                     let response = match method {
                                         http::Method::DELETE => {
-                                            let body = {
+                                            let body = match tokio::time::timeout(HYPER_REQUEST_TIMEOUT, hyper::body::to_bytes(body)).await {
+                                                Ok(result) => match result {
+                                                    Ok(body) => body,
+                                                    Err(err) => return Ok((http_common::server::Error {
+                                                        status_code: http::StatusCode::BAD_REQUEST,
+                                                        message: http_common::server::error_to_message(&err).into(),
+                                                    }).to_http_response()),
+                                                },
+                                                Err(timeout_err) => return Ok((http_common::server::Error {
+                                                    status_code: http::StatusCode::REQUEST_TIMEOUT,
+                                                    message: http_common::server::error_to_message(&timeout_err).into(),
+                                                }).to_http_response()),
+                                            };
+
+                                            let body = if body.len() == 0 {
+                                                None
+                                            } else {
                                                 let content_type = headers.get(hyper::header::CONTENT_TYPE).and_then(|value| value.to_str().ok());
+
                                                 match content_type.as_deref() {
                                                     Some("application/json") | None => {
-                                                        let body = match tokio::time::timeout(HYPER_REQUEST_TIMEOUT, hyper::body::to_bytes(body)).await {
-                                                            Ok(result) => match result {
-                                                                Ok(body) => body,
-                                                                Err(err) => return Ok((http_common::server::Error {
-                                                                    status_code: http::StatusCode::BAD_REQUEST,
-                                                                    message: http_common::server::error_to_message(&err).into(),
-                                                                }).to_http_response()),
-                                                            },
-                                                            Err(timeout_err) => return Ok((http_common::server::Error {
-                                                                status_code: http::StatusCode::REQUEST_TIMEOUT,
-                                                                message: http_common::server::error_to_message(&timeout_err).into(),
-                                                            }).to_http_response()),
-                                                        };
-
                                                         let body: <$route as http_common::server::Route>::DeleteBody = match serde_json::from_slice(&body) {
                                                             Ok(body) => body,
                                                             Err(err) => return Ok((http_common::server::Error {
@@ -142,23 +145,27 @@ macro_rules! make_service {
                                         },
 
                                         http::Method::POST => {
-                                            let body = {
+                                            let body = match tokio::time::timeout(HYPER_REQUEST_TIMEOUT, hyper::body::to_bytes(body)).await {
+                                                Ok(result) => match result {
+                                                    Ok(body) => body,
+                                                    Err(err) => return Ok((http_common::server::Error {
+                                                        status_code: http::StatusCode::BAD_REQUEST,
+                                                        message: http_common::server::error_to_message(&err).into(),
+                                                    }).to_http_response()),
+                                                },
+                                                Err(timeout_err) => return Ok((http_common::server::Error {
+                                                    status_code: http::StatusCode::REQUEST_TIMEOUT,
+                                                    message: http_common::server::error_to_message(&timeout_err).into(),
+                                                }).to_http_response()),
+                                            };
+
+                                            let body = if body.len() == 0 {
+                                                None
+                                            } else {
                                                 let content_type = headers.get(hyper::header::CONTENT_TYPE).and_then(|value| value.to_str().ok());
                                                 match content_type.as_deref() {
                                                     Some("application/json") | None => {
-                                                        let body = match tokio::time::timeout(HYPER_REQUEST_TIMEOUT, hyper::body::to_bytes(body)).await {
-                                                            Ok(result) => match result {
-                                                                Ok(body) => body,
-                                                                Err(err) => return Ok((http_common::server::Error {
-                                                                    status_code: http::StatusCode::BAD_REQUEST,
-                                                                    message: http_common::server::error_to_message(&err).into(),
-                                                                }).to_http_response()),
-                                                            },
-                                                            Err(timeout_err) => return Ok((http_common::server::Error {
-                                                                status_code: http::StatusCode::REQUEST_TIMEOUT,
-                                                                message: http_common::server::error_to_message(&timeout_err).into(),
-                                                            }).to_http_response()),
-                                                        };
+
 
                                                         let body: <$route as http_common::server::Route>::PostBody = match serde_json::from_slice(&body) {
                                                             Ok(body) => body,
