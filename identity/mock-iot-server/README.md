a0f67041b07a023c3dc90bab8d1c5de4c22006c8
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-01-29 17:45:22 +0000

    Rename mock-dps-server to mock-iot-server and add Hub mocking

---
+++ identity/mock-iot-server/README.md
@@ -0,0 +1,19 @@
+# mock-iot-server
+
+mock-iot-server provides a subset of DPS server and IoT Hub functionality for testing. In its current form, it is *not* a substitute for testing with the real IoT.
+ - Does not verify any client credentials
+ - May return the same hardcoded responses to all clients
+ - May panic on error
+
+## Arguments
+
+Required arguments:
+ - `--port`: Port to listen on. Hostname is always `localhost`.
+ - `--server-cert-chain`: Path to server cert chain, starting with the leaf and ending with the root.
+ - `--server-key`: Server cert key.
+
+## TLS server certificate
+
+Since Azure only accepts requests over TLS, mock-iot-server needs a TLS server certificate. See [mock-iot-cert-gen.sh](../../ci/mock-iot-tests/mock-iot-cert-gen.sh) for an example on how to generate a root CA certificate and TLS server certificate for mock-iot-server.
+
+mock-iot-server's root CA certificate must be installed to the system's root CA certificate store.
--- identity/mock-iot-server/src/dps.rs
+++ identity/mock-iot-server/src/dps.rs
@@ -2,15 +2,16 @@
 
 use crate::server::Response;
 
+#[allow(clippy::too_many_lines)]
 fn register(
     registration_id: String,
     headers: &std::collections::HashMap<String, String>,
-    body: Option<String>,
-    context: &mut crate::server::DpsContext,
+    body: &Option<String>,
+    context: &mut crate::server::Context,
 ) -> Response {
     let body = if let Some(body) = body {
         let body: aziot_dps_client_async::model::DeviceRegistration =
-            match serde_json::from_str(&body) {
+            match serde_json::from_str(body) {
                 Ok(body) => body,
                 Err(_) => return Response::bad_request("failed to parse register body"),
             };
@@ -26,42 +27,44 @@ fn register(
         return Response::bad_request("missing required body for register");
     };
 
+    let mut context = context.lock().unwrap();
+
     // Unique value to use for both operation ID and device ID.
     let uuid = uuid::Uuid::new_v4().to_hyphenated().to_string();
-
     let now = chrono::Utc::now().to_rfc3339_opts(chrono::SecondsFormat::Micros, true);
+
     let mut registration_state = aziot_dps_client_async::model::DeviceRegistrationResult {
         tpm: None,
         x509: None,
         symmetric_key: None,
         registration_id: Some(registration_id),
         created_date_time_utc: Some(now.clone()),
-        // Use localhost as hubname so devices provisioned with mock-dps-server don't try to
+        // Use localhost as hubname so devices provisioned with mock-iot-server don't try to
         // communicate with IoT Hub.
-        assigned_hub: Some("localhost".to_string()),
+        assigned_hub: Some(context.endpoint.clone()),
         device_id: Some(uuid.clone()),
         status: Some("assigned".to_string()),
         substatus: Some("initialAssignment".to_string()),
         error_code: None,
         error_message: None,
         last_updated_date_time_utc: Some(now),
-        etag: Some("mock-dps-etag".to_string()),
+        etag: Some("mock-iot-etag".to_string()),
     };
 
     if body.tpm.is_some() {
         registration_state.tpm = Some(aziot_dps_client_async::model::TpmRegistrationResult {
-            authentication_key: "mock-dps-tpm-key".to_string(),
+            authentication_key: "mock-iot-tpm-key".to_string(),
         });
     } else if headers.get("authorization").is_some() {
         registration_state.symmetric_key = Some(
             aziot_dps_client_async::model::SymmetricKeyRegistrationResult {
-                enrollment_group_id: Some("mock-dps-enrollment-group".to_string()),
+                enrollment_group_id: Some("mock-iot-enrollment-group".to_string()),
             },
         );
     } else {
         registration_state.x509 = Some(aziot_dps_client_async::model::X509RegistrationResult {
             certificate_info: None,
-            enrollment_group_id: Some("mock-dps-enrollment-group".to_string()),
+            enrollment_group_id: Some("mock-iot-enrollment-group".to_string()),
             signing_certificate_info: None,
         });
     };
@@ -69,11 +72,10 @@ fn register(
     let operation_id = {
         let registration = aziot_dps_client_async::model::RegistrationOperationStatus {
             operation_id: uuid.clone(),
-            status: "assigned".to_string(),
+            status: registration_state.status.clone().unwrap(),
             registration_state: Some(registration_state),
         };
 
-        let mut context = context.lock().unwrap();
         context
             .in_progress_operations
             .insert(uuid.clone(), registration);
@@ -90,33 +92,38 @@ fn register(
     Response::json(hyper::StatusCode::OK, response)
 }
 
-fn operation_status(operation_id: &str, context: &mut crate::server::DpsContext) -> Response {
+fn operation_status(operation_id: &str, context: &mut crate::server::Context) -> Response {
     let mut context = context.lock().unwrap();
 
     match context.in_progress_operations.remove(operation_id) {
-        Some(operation) => Response::json(hyper::StatusCode::OK, operation),
+        Some(operation) => {
+            // Add new device with empty module set for successful registrations.
+            if operation.status == "assigned" {
+                let device_id = operation
+                    .registration_state
+                    .clone()
+                    .unwrap()
+                    .device_id
+                    .unwrap();
+
+                context
+                    .devices
+                    .insert(device_id, std::collections::BTreeSet::new());
+            }
+
+            Response::json(hyper::StatusCode::OK, operation)
+        }
         None => Response::not_found(format!("operation {} not found", operation_id)),
     }
 }
 
-fn get_param(captures: &regex::Captures<'_>, name: &str) -> Result<String, Response> {
-    let value = &captures[name];
-
-    let value = percent_encoding::percent_decode_str(value)
-        .decode_utf8()
-        .map_err(|_| Response::bad_request(format!("bad {}", name)))?
-        .to_string();
-
-    Ok(value)
-}
-
-pub(crate) fn process_dps_request(
-    req: crate::server::ParsedRequest,
-    context: &mut crate::server::DpsContext,
-) -> Response {
+pub(crate) fn process_request(
+    req: &crate::server::ParsedRequest,
+    context: &mut crate::server::Context,
+) -> Option<Response> {
     lazy_static::lazy_static! {
         static ref DPS_REGEX: regex::Regex = regex::Regex::new(
-            "/(?P<scopeId>[^/]+)/registrations/(?P<registrationId>[^/]+)/(?P<action>.+)\\?api-version=\\d{4}-\\d{2}-\\d{2}$"
+            "/(?P<scopeId>[^/]+)/registrations/(?P<registrationId>[^/]+)/(?P<action>.+)\\?api-version="
         ).unwrap();
 
         static ref OPERATION_STATUS_REGEX: regex::Regex = regex::Regex::new(
@@ -125,40 +132,40 @@ pub(crate) fn process_dps_request(
     }
 
     if !DPS_REGEX.is_match(&req.uri) {
-        return Response::not_found(format!("{} not found", req.uri));
+        return None;
     }
 
     let captures = DPS_REGEX.captures(&req.uri).unwrap();
 
-    let registration_id = match get_param(&captures, "registrationId") {
+    let registration_id = match crate::server::get_param(&captures, "registrationId") {
         Ok(registration_id) => registration_id,
-        Err(response) => return response,
+        Err(response) => return Some(response),
     };
 
-    let action = match get_param(&captures, "action") {
+    let action = match crate::server::get_param(&captures, "action") {
         Ok(action) => action,
-        Err(response) => return response,
+        Err(response) => return Some(response),
     };
 
     if OPERATION_STATUS_REGEX.is_match(&action) {
         if req.method != hyper::Method::GET {
-            return Response::method_not_allowed(&req.method);
+            return Some(Response::method_not_allowed(&req.method));
         }
 
         let captures = OPERATION_STATUS_REGEX.captures(&action).unwrap();
-        let operation_id = match get_param(&captures, "operationId") {
+        let operation_id = match crate::server::get_param(&captures, "operationId") {
             Ok(operation_id) => operation_id,
-            Err(response) => return response,
+            Err(response) => return Some(response),
         };
 
-        operation_status(&operation_id, context)
+        Some(operation_status(&operation_id, context))
     } else if action == "register" {
         if req.method != hyper::Method::PUT {
-            return Response::method_not_allowed(&req.method);
+            return Some(Response::method_not_allowed(&req.method));
         }
 
-        register(registration_id, &req.headers, req.body, context)
+        Some(register(registration_id, &req.headers, &req.body, context))
     } else {
-        Response::not_found(format!("{} not found", req.uri))
+        Some(Response::not_found(format!("{} not found", req.uri)))
     }
 }
