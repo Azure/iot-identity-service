9abef72a6e60c98663b200903f8ee98952ab4424
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-02-02 20:00:09 +0000

    Change openssl types to Ref

--- openssl2/src/extension.rs
+++ openssl2/src/extension.rs
@@ -14,7 +14,7 @@ pub struct ExtKeyUsage {
 
 impl ExtKeyUsage {
     /// Parse an extension as `extKeyUsage`.
-    pub fn from_ext(ext: &openssl::x509::X509Extension) -> Option<Self> {
+    pub fn from_ext(ext: &openssl::x509::X509ExtensionRef) -> Option<Self> {
         let (name, data) = parse_extension(ext);
 
         if name != openssl::nid::Nid::EXT_KEY_USAGE {
@@ -53,16 +53,16 @@ impl ExtKeyUsage {
 }
 
 fn parse_extension(
-    ext: &openssl::x509::X509Extension,
+    ext: &openssl::x509::X509ExtensionRef,
 ) -> (openssl::nid::Nid, &openssl::asn1::Asn1BitStringRef) {
-    let (obj, data): (openssl::asn1::Asn1Object, _) = unsafe {
-        let ptr = foreign_types_shared::ForeignType::as_ptr(ext);
+    let (obj, data): (&openssl::asn1::Asn1ObjectRef, _) = unsafe {
+        let ptr = foreign_types_shared::ForeignTypeRef::as_ptr(ext);
 
         let obj = openssl_sys2::X509_EXTENSION_get_object(ptr);
         let data = openssl_sys2::X509_EXTENSION_get_data(ptr);
 
         (
-            foreign_types_shared::ForeignType::from_ptr(obj),
+            foreign_types_shared::ForeignTypeRef::from_ptr(obj),
             foreign_types_shared::ForeignTypeRef::from_ptr(data),
         )
     };
