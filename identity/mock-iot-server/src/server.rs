d136a2b1562f25aa39136a9d8f4b5515e9b64c92
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-02-03 22:15:40 +0000

    Change mock-iot-server to use HashSet to store modules

--- identity/mock-iot-server/src/server.rs
+++ identity/mock-iot-server/src/server.rs
@@ -139,7 +139,7 @@ pub(crate) struct ContextInner {
     >,
     pub devices: std::collections::BTreeMap<
         String,
-        std::collections::BTreeSet<aziot_identity_common::hub::Module>,
+        std::collections::HashSet<aziot_identity_common::hub::Module>,
     >,
     pub endpoint: String,
 }
