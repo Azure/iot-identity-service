0181b9d153e721e5a1a8665a671a8f765df7fca2
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-02-04 12:38:08 -0800

    Add Hub mocking to mock-iot-server (#357) (#359)
    - Renames mock-dps-server to mock-iot-server.
    - Adds capability to mock Hub get_modules requests. This allow the full provisioning and identity reconciliation flow to be tested.

--- ci/mock-iot-tests/mock-iot-provision.sh
+++ ci/mock-iot-tests/mock-iot-provision.sh
@@ -4,81 +4,16 @@ set -euo pipefail
 
 cd /src
 . ./ci/install-runtime-deps.sh
-. ./ci/mock-dps-tests/mock-dps-root-install.sh
+. ./ci/mock-iot-tests/mock-iot-setup.sh
 
-# Find the build output directory / the directory where CI extracted the artifact.
-#
-# For a local build, this would be target/x86_64-unknown-linux-gnu/debug.
-# For CI, this would be target/debug.
-cd "$(find target -type f -name mock-dps-server | head -n 1 | xargs dirname)"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$PWD"
-
-chmod +x ./aziotd
-chmod +x ./mock-dps-server
-
-# Create directories needed for the tests.
-mkdir -p /run/aziot
-mkdir -p /etc/aziot/keyd/
-mkdir -p /var/lib/aziot/keyd
-mkdir -p /etc/aziot/certd
-mkdir -p /var/lib/aziot/certd
-mkdir -p /etc/aziot/identityd
-mkdir -p /var/lib/aziot/identityd
-
-uid=$(id -u)
-
-# Start mock-dps-server and wait for it to come up.
-./mock-dps-server --port 8443 --server-cert-chain "$SERVER_CERT_CHAIN" --server-key "$SERVER_KEY" &
+# Start mock-iot-server and wait for it to come up.
+./mock-iot-server --port 8443 --server-cert-chain "$SERVER_CERT_CHAIN" --server-key "$SERVER_KEY" &
 server_pid="$!"
 sleep 1
 
-# mock-dps-server does not authenticate, so tests can use any arbitrary credentials.
-echo 'mock-dps-provision' | base64 > device_id_symkey
-
-touch ~/.rnd
->device_id_cert.conf cat <<-EOF
-[ device_id_cert ]
-basicConstraints = CA:FALSE
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment
-extendedKeyUsage = clientAuth, emailProtection
-EOF
-
-openssl genrsa -out device_id_certkey.pem
-openssl req -new -key device_id_certkey.pem -subj "/CN=mock-dps-provision" -out device_id_certreq.pem
-
-openssl x509 -req \
-    -in device_id_certreq.pem \
-    -extfile device_id_cert.conf -extensions device_id_cert \
-    -signkey device_id_certkey.pem -sha256 \
-    -out device_id_cert.pem
-
->/etc/aziot/keyd/config.toml cat <<-EOF
-[aziot_keys]
-homedir_path = "/var/lib/aziot/keyd"
-
-[preloaded_keys]
-device-id-symkey = "file://$PWD/device_id_symkey"
-device-id-certkey = "file://$PWD/device_id_certkey.pem"
-
-[[principal]]
-uid = $uid
-keys = ["*"]
-EOF
-
->/etc/aziot/certd/config.toml cat<<-EOF
-homedir_path = "/var/lib/aziot/certd"
-
-[preloaded_certs]
-device-id-cert = "file://$PWD/device_id_cert.pem"
-
-[[principal]]
-uid = $uid
-certs = ["*"]
-EOF
-
 # Set up for DPS provisioning with symmetric key.
 >/etc/aziot/identityd/config.toml cat<<-EOF
-hostname = "$(hostname)"
+hostname = "$HOSTNAME"
 homedir = "/var/lib/aziot/identityd"
 
 [provisioning]
@@ -88,11 +23,11 @@ scope_id = "scope123"
 
 [provisioning.attestation]
 method = "symmetric_key"
-registration_id = "mock-dps-provision"
+registration_id = "mock-iot-provision"
 symmetric_key = "device-id-symkey"
 
 [[principal]]
-uid = $uid
+uid = $UID
 name = "aziot-edged"
 EOF
 
@@ -123,13 +58,13 @@ symkey_device_id=$(jq .spec.deviceId <<< "$device_id_response")
 
 if [ "$auth_type" != '"sas"' ]; then
     echo ""
-    echo "SYMMETRIC KEY PROVISIONING WITH MOCK DPS: FAIL"
+    echo "SYMMETRIC KEY PROVISIONING WITH MOCK IoT: FAIL"
     echo "Auth type is $auth_type, not 'sas'"
     exit 1
 fi
 
 echo ""
-echo "SYMMETRIC KEY PROVISIONING WITH MOCK DPS: PASS"
+echo "SYMMETRIC KEY PROVISIONING WITH MOCK IoT: PASS"
 echo ""
 
 # Change to DPS provisioning with X.509 credential.
@@ -137,7 +72,7 @@ kill -TERM "$identityd_pid"
 wait "$identityd_pid" || :
 
 >/etc/aziot/identityd/config.toml cat<<-EOF
-hostname = "$(hostname)"
+hostname = "$HOSTNAME"
 homedir = "/var/lib/aziot/identityd"
 
 [provisioning]
@@ -147,18 +82,22 @@ scope_id = "scope123"
 
 [provisioning.attestation]
 method = "x509"
-registration_id = "mock-dps-provision"
+registration_id = "mock-iot-provision"
 identity_cert = "device-id-cert"
 identity_pk = "device-id-certkey"
 
 [[principal]]
-uid = $uid
+uid = $UID
 name = "aziot-edged"
 EOF
 
 ./aziotd aziot-identityd &
 identityd_pid="$!"
 sleep 5
+
+# Provisioning info has changed, so reprovision.
+curl -s --unix-socket /run/aziot/identityd.sock "http://localhost/identities/device/reprovision?api-version=2020-09-01" \
+    -H "content-type: application/json" --data '{"type": "aziot"}' &> /dev/null
 
 # Get device identity. Check that it changed with the reprovision.
 device_id_response=$(curl -s \
@@ -174,21 +113,21 @@ x509_device_id=$(jq .spec.deviceId <<< "$device_id_response")
 
 if [ "$auth_type" != '"x509"' ]; then
     echo ""
-    echo "X.509 PROVISIONING WITH MOCK DPS: FAIL"
+    echo "X.509 PROVISIONING WITH MOCK IoT: FAIL"
     echo "Auth type is $auth_type, not 'x509'"
     exit 1
 fi
 
 if [ "$symkey_device_id" = "$x509_device_id" ]; then
     echo ""
-    echo "MOCK DPS REPROVISION: FAIL"
+    echo "MOCK IoT REPROVISION: FAIL"
     echo "Device identity did not change."
     echo ""
     exit 1
 fi
 
 echo ""
-echo "X.509 PROVISIONING WITH MOCK DPS: PASS"
+echo "X.509 PROVISIONING WITH MOCK IoT: PASS"
 echo ""
 
 # Clean up.
