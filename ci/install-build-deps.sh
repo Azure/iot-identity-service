0b6fd3d222d9cc97d1ce6c015c226be033b9f2b7
Joseph Knierman <joknierm@microsoft.com>
2022-03-01 16:36:13 -0800

    fixed syntax issue

--- ci/install-build-deps.sh
+++ ci/install-build-deps.sh
@@ -139,13 +139,12 @@ case "$OS:$ARCH" in
         if [ -f /.dockerenv ]; then
             mv /.dockerenv /.dockerenv.old
         fi
-        BranchTag='1.0-stable'
         case "$OS" in
             'mariner:1')
-                $BranchTag='1.0-stable'
+                BranchTag='1.0-stable'
                 ;;
             'mariner:2')
-                $BranchTag='2.0-stable'
+                BranchTag='2.0-stable'
                 ;;
         esac
 
