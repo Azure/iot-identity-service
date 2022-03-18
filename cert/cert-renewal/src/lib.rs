35b7cdb669501a545b3ee7ebc53cb9a691df6179
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-03-18 10:16:07 -0700

    Add certificate renewal engine (#378)
    Adds the shared library that manages certificate renewal.

--- cert/cert-renewal/src/lib.rs
+++ cert/cert-renewal/src/lib.rs
@@ -8,6 +8,40 @@
     clippy::must_use_candidate
 )]
 
-pub mod policy;
+mod cert_interface;
+pub use cert_interface::CertInterface;
 
+#[cfg(test)]
+use cert_interface::TestInterface;
+
+mod credential;
+use credential::{Credential, CredentialHeap};
+
+pub mod engine;
+pub use engine::RenewalEngine;
+
+pub mod error;
+pub use error::Error;
+
+pub mod policy;
 pub use policy::{Policy, RenewalPolicy};
+
+mod time;
+use time::Time;
+
+#[cfg(test)]
+use time::test_time;
+
+/// Common function for generating test credentials.
+#[cfg(test)]
+fn test_cert(not_before: i64, not_after: i64) -> openssl::x509::X509 {
+    let (cert, _) = test_common::credential::custom_test_certificate("test_cert", |cert| {
+        let not_before = openssl::asn1::Asn1Time::from_unix(not_before).unwrap();
+        let not_after = openssl::asn1::Asn1Time::from_unix(not_after).unwrap();
+
+        cert.set_not_before(&not_before).unwrap();
+        cert.set_not_after(&not_after).unwrap();
+    });
+
+    cert
+}
--- cert/cert-renewal/src/policy.rs
+++ cert/cert-renewal/src/policy.rs
@@ -21,9 +21,8 @@ pub enum Policy {
 impl Policy {
     /// Check if the given cert should be renewed based on this policy.
     pub fn should_renew(&self, cert: &openssl::x509::X509) -> bool {
-        let not_before = cert.not_before();
-        let not_after = cert.not_after();
-        let now = now();
+        let not_before = crate::Time::from(cert.not_before());
+        let not_after = crate::Time::from(cert.not_after());
 
         // Cert not_after must be after not_before to be valid.
         // Always renew invalid certs.
@@ -32,29 +31,21 @@ impl Policy {
         }
 
         // Check for expired certificate. Renew if expired.
-        if now >= not_after {
+        if not_after.in_past() {
             return true;
         }
 
         match self {
             Policy::Percentage(threshold) => {
-                let total_lifetime = not_before.diff(not_after).expect("valid times should diff");
-                let total_lifetime = timediff_to_secs(&total_lifetime);
-
-                let current_lifetime = not_before.diff(&now).expect("valid times should diff");
-                let current_lifetime = timediff_to_secs(&current_lifetime);
+                let total_lifetime = not_after - not_before;
+                let current_lifetime = crate::Time::now() - not_before;
 
                 let lifetime = current_lifetime * 100 / total_lifetime;
 
                 lifetime >= *threshold
             }
 
-            Policy::Time(threshold) => {
-                let expiry = asn1time_to_unix(not_after);
-                let now = asn1time_to_unix(&now);
-
-                expiry - now <= *threshold
-            }
+            Policy::Time(threshold) => not_after - crate::Time::now() <= *threshold,
         }
     }
 }
@@ -147,57 +138,11 @@ impl serde::Serialize for Policy {
     }
 }
 
-fn asn1time_to_unix(time: &openssl::asn1::Asn1TimeRef) -> i64 {
-    let epoch = openssl::asn1::Asn1Time::from_unix(0).expect("unix epoch should be valid");
-    let unix = epoch.diff(time).expect("valid times should diff");
-
-    i64::from(unix.days) * 86400 + i64::from(unix.secs)
-}
-
-fn timediff_to_secs(diff: &openssl::asn1::TimeDiff) -> i64 {
-    // This function is only called for diffs that should be non-negative.
-    assert!(diff.days >= 0);
-    assert!(diff.secs >= 0);
-
-    i64::from(diff.days) * 86400 + i64::from(diff.secs)
-}
-
-#[cfg(not(test))]
-fn now() -> openssl::asn1::Asn1Time {
-    openssl::asn1::Asn1Time::days_from_now(0).expect("current time should be valid")
-}
-
-// Mocks the current time for testing.
-#[cfg(test)]
-fn now() -> openssl::asn1::Asn1Time {
-    let time = tests::CURRENT_TIME.load(std::sync::atomic::Ordering::Acquire);
-
-    openssl::asn1::Asn1Time::from_unix(time).expect("invalid time")
-}
-
 #[cfg(test)]
 mod tests {
+    use crate::test_cert;
+
     use super::Policy;
-
-    // Tests that modify this value must be run serially.
-    pub(super) static CURRENT_TIME: std::sync::atomic::AtomicI64 =
-        std::sync::atomic::AtomicI64::new(0);
-
-    fn set_time(time: i64) {
-        CURRENT_TIME.store(time, std::sync::atomic::Ordering::Release);
-    }
-
-    fn generate_cert(not_before: i64, not_after: i64) -> openssl::x509::X509 {
-        let (cert, _) = test_common::credential::custom_test_certificate("test_cert", |cert| {
-            let not_before = openssl::asn1::Asn1Time::from_unix(not_before).unwrap();
-            let not_after = openssl::asn1::Asn1Time::from_unix(not_after).unwrap();
-
-            cert.set_not_before(&not_before).unwrap();
-            cert.set_not_after(&not_after).unwrap();
-        });
-
-        cert
-    }
 
     #[test]
     fn deserialize_ok() {
@@ -281,7 +226,7 @@ mod tests {
     #[test]
     fn bad_cert() {
         // Bad cert: not_after is before not_before.
-        let cert = generate_cert(5, 1);
+        let cert = test_cert(5, 1);
 
         let policy = Policy::Percentage(100);
         assert!(policy.should_renew(&cert));
@@ -291,8 +236,8 @@ mod tests {
     #[serial_test::serial]
     fn expired_cert() {
         // Cert expired at t=5 when current time is t=6.
-        let cert = generate_cert(1, 5);
-        set_time(6);
+        let cert = test_cert(1, 5);
+        crate::test_time::set(6);
 
         let policy = Policy::Percentage(100);
         assert!(policy.should_renew(&cert));
@@ -301,9 +246,11 @@ mod tests {
     #[test]
     #[serial_test::serial]
     fn policy_percentage() {
+        crate::test_time::reset();
+
         // Test calculation with negative timestamps.
-        let cert = generate_cert(-5, -1);
-        set_time(-3);
+        let cert = test_cert(-5, -1);
+        crate::test_time::set(-3);
 
         let policy = Policy::Percentage(40);
         assert!(policy.should_renew(&cert));
@@ -315,8 +262,8 @@ mod tests {
         assert!(!policy.should_renew(&cert));
 
         // Test calculation with mixed timestamps.
-        let cert = generate_cert(-2, 2);
-        set_time(0);
+        let cert = test_cert(-2, 2);
+        crate::test_time::reset();
 
         let policy = Policy::Percentage(40);
         assert!(policy.should_renew(&cert));
@@ -328,15 +275,13 @@ mod tests {
         assert!(!policy.should_renew(&cert));
 
         // Test calculation with positive timestamps.
-        let cert = generate_cert(1, 5);
-        set_time(3);
+        let cert = test_cert(1, 5);
+        crate::test_time::set(3);
 
         let policy = Policy::Percentage(40);
         assert!(policy.should_renew(&cert));
-
         let policy = Policy::Percentage(50);
         assert!(policy.should_renew(&cert));
-
         let policy = Policy::Percentage(60);
         assert!(!policy.should_renew(&cert));
     }
@@ -345,8 +290,8 @@ mod tests {
     #[serial_test::serial]
     fn policy_time() {
         // Test calculation with negative timestamps.
-        let cert = generate_cert(-5, -1);
-        set_time(-3);
+        let cert = test_cert(-5, -1);
+        crate::test_time::set(-3);
 
         // Renewal policy of "within 1": should not renew.
         let policy = Policy::Time(1);
@@ -357,8 +302,8 @@ mod tests {
         assert!(policy.should_renew(&cert));
 
         // Test calculation with mixed timestamps.
-        let cert = generate_cert(-2, 2);
-        set_time(0);
+        let cert = test_cert(-2, 2);
+        crate::test_time::reset();
 
         // Renewal policy of "within 1": should not renew.
         let policy = Policy::Time(1);
@@ -369,8 +314,8 @@ mod tests {
         assert!(policy.should_renew(&cert));
 
         // Test calculation with positive timestamps.
-        let cert = generate_cert(1, 5);
-        set_time(3);
+        let cert = test_cert(1, 5);
+        crate::test_time::set(3);
 
         // Renewal policy of "within 1": should not renew.
         let policy = Policy::Time(1);
