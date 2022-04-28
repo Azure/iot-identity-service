aa311a83ba5f6d1d00eb4b0fb6cd28198092ecae
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-04-28 17:57:30 +0000

    Change cert renewal to use cert chains

--- cert/aziot-certd/src/renewal.rs
+++ cert/aziot-certd/src/renewal.rs
@@ -102,13 +102,13 @@ impl cert_renewal::CertInterface for EstIdRenewal {
     async fn get_cert(
         &mut self,
         _cert_id: &str,
-    ) -> Result<openssl::x509::X509, cert_renewal::Error> {
+    ) -> Result<Vec<openssl::x509::X509>, cert_renewal::Error> {
         let cert = std::fs::read(&self.path).map_err(|err| {
-            cert_renewal::Error::retryable_error(format!("failed to read cert: {}", err))
+            cert_renewal::Error::retryable_error(format!("failed to read cert chain: {}", err))
         })?;
 
-        openssl::x509::X509::from_pem(&cert)
-            .map_err(|_| cert_renewal::Error::fatal_error("failed to parse cert"))
+        openssl::x509::X509::stack_from_pem(&cert)
+            .map_err(|_| cert_renewal::Error::fatal_error("failed to parse cert chain"))
     }
 
     async fn get_key(
@@ -133,15 +133,15 @@ impl cert_renewal::CertInterface for EstIdRenewal {
 
     async fn renew_cert(
         &mut self,
-        old_cert: &openssl::x509::X509,
+        old_cert_chain: &[openssl::x509::X509],
         key_id: &str,
-    ) -> Result<(openssl::x509::X509, Self::NewKey), cert_renewal::Error> {
+    ) -> Result<(Vec<openssl::x509::X509>, Self::NewKey), cert_renewal::Error> {
         // If the old cert is expired, authenticate with the bootstrap credentials. Otherwise,
         // use the old cert to authenticate.
         let now = openssl::asn1::Asn1Time::days_from_now(0)
             .map_err(|_| cert_renewal::Error::retryable_error("failed to get current time"))?;
 
-        let (est_id_cert, est_id_key) = if old_cert.not_after() <= now {
+        let (est_id_cert, est_id_key) = if old_cert_chain[0].not_after() <= now {
             if let Some((bootstrap_path, bootstrap_key)) = &self.bootstrap_path {
                 let bootstrap_cert = std::fs::read(bootstrap_path).map_err(|_| {
                     cert_renewal::Error::retryable_error("failed to read bootstrap cert")
@@ -154,7 +154,7 @@ impl cert_renewal::CertInterface for EstIdRenewal {
                 ));
             }
         } else {
-            let cert = old_cert
+            let cert = old_cert_chain[0]
                 .to_pem()
                 .map_err(|_| cert_renewal::Error::fatal_error("bad cert"))?;
 
@@ -206,7 +206,7 @@ impl cert_renewal::CertInterface for EstIdRenewal {
             let est_config = self.est_config.read().await;
 
             crate::create_est_id(
-                old_cert.subject_name(),
+                old_cert_chain[0].subject_name(),
                 keys,
                 &self.url,
                 (est_id_cert, est_id_key),
@@ -223,31 +223,43 @@ impl cert_renewal::CertInterface for EstIdRenewal {
             })?
         };
 
-        let cert = openssl::x509::X509::from_pem(&cert)
+        let cert_chain = openssl::x509::X509::stack_from_pem(&cert)
             .map_err(|_| cert_renewal::Error::retryable_error("failed to parse new cert"))?;
 
-        Ok((cert, key_id))
+        Ok((cert_chain, key_id))
     }
 
     async fn write_credentials(
         &mut self,
-        old_cert: &openssl::x509::X509,
-        new_cert: (&str, &openssl::x509::X509),
+        old_cert_chain: &[openssl::x509::X509],
+        new_cert_chain: (&str, &[openssl::x509::X509]),
         key: (&str, Self::NewKey),
     ) -> Result<(), cert_renewal::Error> {
-        let new_cert = new_cert.1;
+        let new_cert_chain = new_cert_chain.1;
         let (old_key, new_key) = (key.0, key.1);
 
-        let new_cert_pem = new_cert
-            .to_pem()
-            .map_err(|_| cert_renewal::Error::retryable_error("bad cert"))?;
+        let mut new_cert_chain_pem = Vec::new();
 
-        let old_cert_pem = old_cert
-            .to_pem()
-            .map_err(|_| cert_renewal::Error::retryable_error("bad cert"))?;
+        for cert in new_cert_chain {
+            let mut cert = cert
+                .to_pem()
+                .map_err(|_| cert_renewal::Error::retryable_error("bad cert"))?;
+
+            new_cert_chain_pem.append(&mut cert);
+        }
+
+        let mut old_cert_chain_pem = Vec::new();
+
+        for cert in old_cert_chain {
+            let mut cert = cert
+                .to_pem()
+                .map_err(|_| cert_renewal::Error::retryable_error("bad cert"))?;
+
+            old_cert_chain_pem.append(&mut cert);
+        }
 
         // Commit the new cert to storage.
-        std::fs::write(&self.path, &new_cert_pem)
+        std::fs::write(&self.path, &new_cert_chain_pem)
             .map_err(|_| cert_renewal::Error::retryable_error("failed to import new cert"))?;
 
         // Commit the new key to storage if the key was rotated.
@@ -259,7 +271,7 @@ impl cert_renewal::CertInterface for EstIdRenewal {
                 .is_err()
         {
             // Revert to the previous cert if the key could not be written.
-            std::fs::write(&self.path, &old_cert_pem)
+            std::fs::write(&self.path, &old_cert_chain_pem)
                 .map_err(|_| cert_renewal::Error::retryable_error("failed to restore old cert"))?;
         }
 
