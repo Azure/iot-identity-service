0c83134465ba094973106c9aefecdc9a2d54ba0c
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-05-06 22:40:34 +0000

    Revert changes to openssl2

--- openssl-sys2/src/x509.rs
+++ openssl-sys2/src/x509.rs
@@ -7,12 +7,4 @@ extern "C" {
         x509: *const openssl_sys::X509,
         pkey: *const openssl_sys::EVP_PKEY,
     ) -> std::os::raw::c_int;
-
-    pub fn X509_EXTENSION_get_data(
-        ext: *mut openssl_sys::X509_EXTENSION,
-    ) -> *mut openssl_sys::ASN1_BIT_STRING;
-
-    pub fn X509_EXTENSION_get_object(
-        ext: *mut openssl_sys::X509_EXTENSION,
-    ) -> *mut openssl_sys::ASN1_OBJECT;
 }
