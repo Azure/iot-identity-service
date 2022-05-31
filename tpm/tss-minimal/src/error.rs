9aff814623d30651118fcd51457fd25a10e52780
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-05-31 16:08:43 -0700

    Remove unnecessary mutex in tss_rc_decode

--- tpm/tss-minimal/src/error.rs
+++ tpm/tss-minimal/src/error.rs
@@ -1,30 +1,15 @@
 use std::ffi::CStr;
 use std::fmt;
-use std::sync::Mutex;
 
 pub fn try_decode_rc(rc: u32) -> Option<String> {
-    // Tss2_RC_Decode uses thread-local storage, and overwrites the
-    // return address on each invocation.  So, we have to make sure to
-    // save the error message contents before the next invocation on
-    // this thread.
-    thread_local! {
-        static MUTEX: Mutex<()> = Mutex::new(());
+    let msg = unsafe { rc_sys::Tss2_RC_Decode(rc) };
+
+    if msg.is_null() {
+        return None;
     }
 
-    MUTEX.with(|m| {
-        // There are technically no invariants to protect, so another
-        // option is to replace `.expect(...)` with
-        // `.unwrap_or_else(std::sync::PoisonError::into_inner)`.
-        let _lock = m.lock().expect("poisoned mutex");
-        let msg = unsafe { rc_sys::Tss2_RC_Decode(rc) };
-
-        if msg.is_null() {
-            return None;
-        }
-
-        let cstr = unsafe { CStr::from_ptr(msg) };
-        cstr.to_str().ok().map(ToOwned::to_owned)
-    })
+    let cstr = unsafe { CStr::from_ptr(msg) };
+    cstr.to_str().ok().map(ToOwned::to_owned)
 }
 
 #[macro_export]
