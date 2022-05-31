fcddb4ee16e6b043e95b0790ca2da46f385d56d6
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-05-31 15:43:55 -0700

    Change test TCTI configuration and remove old submodules

--- tpm/tss-minimal/tests/create_primary.rs
+++ tpm/tss-minimal/tests/create_primary.rs
@@ -2,7 +2,7 @@
 fn main() -> tss_minimal::Result<()> {
     let _ = env_logger::builder().is_test(true).try_init();
 
-    let ctx = tss_minimal::EsysContext::new(&std::ffi::CString::new("swtpm:port=8181").unwrap())?;
+    let ctx = tss_minimal::EsysContext::new(&std::ffi::CString::default())?;
 
     let _handle = ctx.create_primary(
         &tss_minimal::AuthSession::PASSWORD,
