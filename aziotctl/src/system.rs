5eca52f495aa0ce12507a52d4cd4f59bc15c2aa6
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-10-24 12:10:04 -0700

    Map parser outputs instead of generating help manually

--- aziotctl/src/system.rs
+++ aziotctl/src/system.rs
@@ -3,6 +3,7 @@
 use std::ffi::{OsStr, OsString};
 
 use anyhow::{anyhow, Result};
+use clap::builder::TypedValueParser;
 
 use aziotctl_common::system::{
     get_status, get_system_logs, restart, set_log_level, stop, SERVICE_DEFINITIONS,
@@ -39,8 +40,8 @@ pub struct LogLevelOptions {
     // `log::Level`:
     // https://github.com/rust-lang/log/blob/d6707108c6959ac7b60cdb60a005795ece6d82d6/src/lib.rs#L481
     #[arg(
-        value_parser,
-        help = "[possible values: error, warn, info, debug, trace]"
+        value_parser = clap::builder::PossibleValuesParser::new(["error", "warn", "info", "debug", "trace"])
+            .try_map(|s| s.parse::<log::Level>()),
     )]
     log_level: log::Level,
 }
