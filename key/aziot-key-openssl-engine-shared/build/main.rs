80d78bf0ea01b6537524fca6950b21ee1e246b27
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2023-01-18 21:16:59 -0800

    Prevent LTO from optimizing out OpenSSL engine symbols
    LTO is enabled by default for .deb packages starting in Ubuntu
    21.04[^1], causing build failures on Ubuntu 22.04.  Previously, we
    hacked around this by compiling outside of the `dpkg-buildpackage`
    environment[^2].  However, this hack stopped working when the `cc` crate
    began emitting `rerun-if-env-changed` in version `1.0.74`[^3].  Hence,
    would need to set `emit_rerun_if_env_changed(false)` for all `cc::Build`
    instances in `aziot-key-openssl-engine-shared` and its dependency graph
    to restore our hack for `cc > 1.0.73`.  This is not remotely practical.
    
    LTO is incompatible with `__asm__(".symver ..")` because it uses the
    linker-script-provided exported symbols to determine what is safe to
    optimize out, and the linker script is naturally unaware of
    manually-exported symbols.  We can work around this by adding
    `__attribute__((used))` to the functions we want to keep in the final
    object.
    
    Another option would be to use GCC's
    `__attribute__((symver("..@")))`[^4] directive, but this relies on too
    new of a toolchain version (GCC 10).
    
    Addendum 1: The fundamental reason for why LTO is a problem for
    `aziot-key-openssl-engine-shared` in the first place is that this crate
    uses what is, in effect, a "symbol stub" to hook Rust code into
    OpenSSL's engine macros.  First, `build/engine.c` declares the engine
    function signature and uses OpenSSL's macros to expand the dynamic
    engine binding.  This file is then compiled (but not linked) into an
    object that will become the dynamic library's public interface.  The way
    this is accomplished is by linking the whole object into the `cdylib`
    (as opposed to only linking referenced functions).  LTO requires us to
    go one step further by preventing the linker from optimizing out symbols
    not declared as globally-exported in `rustc`'s linker script, which does
    not know of the symbol declaration in the stub object.  There is an open
    RFC request for allowing re-export of C symbols from `cdylib` crates:
    https://github.com/rust-lang/rfcs/issues/2771.
    
    Addendum 2: When our supported platforms start shipping with GNU as >=
    2.35 and/or Clang 13, we may want to add `,remove` to the `.symver`
    directive arguments to lift the restriction that
    `aziot-key-openssl-engine-shared` cannot be included in tests[^5].
    
    [^1]: https://wiki.ubuntu.com/ToolChain/LTO
    [^2]: https://github.com/Azure/iot-identity-service/commit/f66c155113cce5089d398376d16b206f7973d6f6
    [^3]: https://github.com/rust-lang/cc-rs/releases/tag/1.0.74
    [^4]: `..@` instead of `..@@` since we do not define a version
        node name, meaning `..@@` leads to symbol duplication.
    [^5]: https://maskray.me/blog/2020-11-26-all-about-symbol-versioning

--- key/aziot-key-openssl-engine-shared/build/main.rs
+++ key/aziot-key-openssl-engine-shared/build/main.rs
@@ -4,7 +4,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 fn main() {
-    println!("cargo:rerun-if-changed=build/engine.c");
+    println!("cargo:rerun-if-changed=build/");
 
     openssl_build::define_version_number_cfg();
 
@@ -13,13 +13,7 @@ fn main() {
         // Since we are going to use the generated archive in a shared
         // library, we need +whole-archive to be set.  See:
         // https://github.com/rust-lang/rust/blob/1.61.0/RELEASES.md#compatibility-notes
-        .cargo_metadata(false)
+        .link_lib_modifier("+whole-archive")
         .file("build/engine.c")
         .compile("aziot_key_openssl_shared_engine_wrapper");
-
-    println!("cargo:rustc-link-lib=static:+whole-archive=aziot_key_openssl_shared_engine_wrapper");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        std::env::var("OUT_DIR").unwrap()
-    );
 }
