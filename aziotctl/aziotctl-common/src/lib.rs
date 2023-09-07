5b3e17cd48cdce6d742638643a6feeeaa90cf178
Damon Barry <damonb@microsoft.com>
2023-09-07 19:57:28 +0000

    Fix rust syntax error

--- aziotctl/aziotctl-common/src/lib.rs
+++ aziotctl/aziotctl-common/src/lib.rs
@@ -142,8 +142,9 @@ fn program_name() -> String {
     std::env::current_exe()
         .expect("Cannot get the exec path")
         .file_name()
-        .and_then(OsStr::to_str)
-        .unwrap_or("<current program>").to_owned()
+        .and_then(std::ffi::OsStr::to_str)
+        .unwrap_or("<current program>")
+        .to_owned()
 }
 
 #[cfg(test)]
