6a0a31e33d48dce16ffae746da9eddc6acff9b67
yophilav <yophilav@microsoft.com>
2023-11-01 16:32:24 -0700

    Clippy fix aziot-identityd

--- identity/aziot-identityd/src/lib.rs
+++ identity/aziot-identityd/src/lib.rs
@@ -5,7 +5,7 @@
 #![allow(
     clippy::default_trait_access,
     clippy::let_and_return,
-    clippy::let_underscore_drop,
+    let_underscore_drop,
     clippy::let_unit_value,
     clippy::missing_errors_doc,
     clippy::module_name_repetitions,
