a957acfeb4d5d277e48cb4093cc7b3ca4cbe176a
Damon Barry <damonb@microsoft.com>
2024-06-17 15:25:06 -0700

    Truncate storage account name if its too long

--- ci/e2e-tests/suite-common.sh
+++ ci/e2e-tests/suite-common.sh
@@ -14,3 +14,7 @@ dps_allocation_function_name='DpsCustomAllocation'
 dps_allocation_functionapp_name="alloc-app-${suite_common_resource_name}"
 foo_devices_iot_hub="${suite_common_resource_name}-foo-devices"
 dps_allocation_storage_account="$(printf '%s' "$suite_id" | tr '[:upper:]' '[:lower:]' | tr -d -C 'a-z0-9')"
+# storage accounts names can't be longer than 24 chars; truncate (from the front of the string) if necessary
+if [[ ${#dps_allocation_storage_account} -gt 24 ]]; then
+    dps_allocation_storage_account="${dps_allocation_storage_account: -24}"
+fi
